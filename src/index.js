fc98426ada3a8a4a926e09155d6b2c7378a75104
Minh An <54472158+NguyenMinhAn898@users.noreply.github.com>
2021-11-20 13:25:48 +0700

    create-bai-2-introducing-jsx

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./css/index.css";
-// import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-// Helloworld
-ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById("root"));
+// 1
+const name = "Nguyen Minh An";
+const element = <h1>Hello, I am {name}</h1>;
+// 2
+const user = {
+  firstName: "Nguyen Minh",
+  lastName: "An",
+};
+function formatName(user) {
+  return user.firstName + " " + user.lastName;
+}
+const element2 = <h1>Hello, My name is {formatName(user)}</h1>;
+// 3
+const element3 = React.createElement(
+  "h1",
+  { className: "text-center" },
+  "Hello, My name is " + formatName(user)
+);
+// render
+// ReactDOM.render(element, document.getElementById("root"));
+
+// ReactDOM.render(element2, document.getElementById("root"));
+
+ReactDOM.render(element3, document.getElementById("root"));
 
 reportWebVitals();
