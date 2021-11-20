4e1c420352fcc002f9edcf8c5238b8479dd06441
Minh An <54472158+NguyenMinhAn898@users.noreply.github.com>
2021-11-20 15:45:54 +0700

    update

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./css/index.css";
 import reportWebVitals from "./reportWebVitals";
+import List from "./List";
+import ListKey from "./Key";
 
-// Helloworld
-ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById("root"));
+const listNumber = [1, 15, 10, 5, 12, 6];
+
+ReactDOM.render(<List numbers={listNumber} />, document.getElementById("root"));
+
+ReactDOM.render(
+  <ListKey numbers={listNumber} />,
+  document.getElementById("root")
+);
 
 reportWebVitals();
