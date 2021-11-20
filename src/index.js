060166e57518332aeb416e6902c56a7503e72a3d
Minh An <54472158+NguyenMinhAn898@users.noreply.github.com>
2021-11-20 13:34:03 +0700

    create-render-element

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import "./css/index.css";
 // import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-// Helloworld
-ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById("root"));
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world! My name is Nguyen Minh An</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById("root"));
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+setInterval(tick, 1000);
+
 reportWebVitals();
