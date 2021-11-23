4b3f5c3e927303a899849d0ae3eb373c92fcba51
Minh An <54472158+NguyenMinhAn898@users.noreply.github.com>
2021-11-23 09:46:28 +0700

    create form react

--- src/index.js
+++ src/index.js
@@ -2,8 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./css/index.css";
 import reportWebVitals from "./reportWebVitals";
+import Form from "./Form";
+import TextAria from "./Textaria";
+import SelectList from "./Select";
 
 // Helloworld
-ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById("root"));
+// ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById("root"));
+
+// ReactDOM.render(<Form />, document.getElementById("root"));
+
+// ReactDOM.render(<TextAria />, document.getElementById("root"));
+
+// ReactDOM.render(<SelectList />, document.getElementById("root"));
+
+// ReactDOM.render(<input value="hi" />, document.getElementById("root"));
+
+setTimeout(function () {
+  ReactDOM.render(<input value={null} />, document.getElementById("root"));
+}, 1000);
 
 reportWebVitals();
